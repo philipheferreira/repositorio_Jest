2e70e246be38b5db69eff340f52b88c8e85ee058
philipheferreira <philipheferreira@gmail.com>
2024-11-27 10:32:25 -0300

    Feat: for and numeros

--- 2 - Usando Matchers/testeGeral.test.js
+++ 2 - Usando Matchers/testeGeral.test.js
@@ -40,3 +40,40 @@ test('adicionando números positivos não é zero', () => {
     }
   });
 
+
+// VERDADE 
+
+/*
+Em testes às vezes você precisa distinguir entre undefined, null e false, mas às 
+vezes você não quer tratar estes de maneira diferente. Jest contém auxiliares 
+que permitem você ser explícito sobre o que quer.
+
+* toBeNull corresponde a apenas null
+* toBeUndefined corresponde a apenas undefined
+* toBeDefined é o oposto de toBeUndefined
+* toBeTruthy combina com qualquer coisa que uma instrução if trata como verdadeiro
+* toBeFalsy combina com qualquer coisa que uma instrução if trata como falso
+
+*/
+
+test('nulo', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+  test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
+
+//Você deve usar o "matcher" que corresponde mais precisamente para o que você deseja que seu código faça.
+
+
